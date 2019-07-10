73550b82f633d483f3ee6b37efe3162f518f5a0e
Timon van Spronsen <timon@Timons-MacBook-Pro.local>
2019-07-10 20:10:34 +0200

    Quit preferences before before changing defaults

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Dock & Dashboard
