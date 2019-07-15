a377da139668c07a58a90e5ebf43b9453b00e386
Timon van Spronsen <timonvanspronsen@outlook.com>
2019-07-15 13:02:33 +0200

    Keep sudo permissions alive in setup-apps script

--- setup-apps.sh
+++ setup-apps.sh
@@ -6,6 +6,12 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `setup-apps.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Apps
